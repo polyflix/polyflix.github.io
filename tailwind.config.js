5593d1fe033a8b627cb71337971ab60910dc4caa
Malo Polese <malo.polese@etu.umontpellier.fr>
2021-12-19 15:26:44 +0100

    feat(#2): Add prettier and update README

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -9,13 +9,13 @@ module.exports = {
         "poly-red": "#e50914",
       },
       maxWidth: {
-        "content": "max-content"
-      }
+        content: "max-content",
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp')
-  ]
-}
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
