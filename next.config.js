f4589b32f5d180810c484cc0bd23d76906b6ec58
Malo Polese <malo.polese@etu.umontpellier.fr>
2021-12-29 21:07:21 +0100

    feat(#7): add custom image loader - cloudinary

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: './'
+  assetPrefix: './',
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/malo-polese/image/upload/v1617964829/Polytech_DO/polyflix-showcase/',
+  },
 };
